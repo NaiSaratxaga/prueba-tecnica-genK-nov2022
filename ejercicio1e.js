a95ef7ca0fb4ca8627cd3c04b0c1a205ebdc1807
Naiara Saratxaga <99652446+NaiSaratxaga@users.noreply.github.com>
2022-11-03 10:35:09 +0100

    feat: implements the exercise1e

--- ejercicio1e.js
+++ ejercicio1e.js
@@ -5,5 +5,13 @@ const body = document.body;
 for (let index = 1; index <= 1000; index++) {
   const button = document.createElement('button');
   button.innerHTML = `botón ${index}`;
+  button.addEventListener(
+    'mouseover',
+    (event) => (button.innerHTML = `botóN ${index}`)
+  );
+  button.addEventListener(
+    'mouseout',
+    (event) => (button.innerHTML = `botón ${index}`)
+  );
   document.body.appendChild(button);
 }
