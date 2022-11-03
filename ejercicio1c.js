a82ddcd85c55a3d2ebf78ee34cc8bbf40a6f6948
Naiara Saratxaga <99652446+NaiSaratxaga@users.noreply.github.com>
2022-11-03 10:15:51 +0100

    feat: includes the number into the buttons text

--- ejercicio1c.js
+++ ejercicio1c.js
@@ -2,8 +2,8 @@
 
 const body = document.body;
 
-for (let index = 0; index < 10; index++) {
+for (let index = 1; index <= 10; index++) {
   const button = document.createElement('button');
-  button.innerHTML = 'botón';
+  button.innerHTML = `botón ${index}`;
   document.body.appendChild(button);
 }
